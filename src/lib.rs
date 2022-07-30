0c63833b0b01ad0df108b133963b977711d4126e
renato <renato.hermoza@pucp.edu.pe>
2022-07-30 02:51:36 -0500

    add print several graphs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,17 @@ use petgraph::{
     dot::Dot,
     visit::{GraphProp, IntoEdgeReferences, IntoNodeReferences, NodeIndexable},
 };
-use std::fmt;
+use std::{collections::HashMap, fmt, fmt::Write};
+
+fn get_graph_viz<G>(graph: G) -> String
+where
+    G: IntoNodeReferences + IntoEdgeReferences + NodeIndexable + GraphProp,
+    G::EdgeWeight: fmt::Debug,
+    G::NodeWeight: fmt::Debug,
+{
+    let dot = Dot::new(graph);
+    format!("{:?}", dot)
+}
 
 pub fn print_graph_svg<G>(graph: G)
 where
@@ -10,6 +20,72 @@ where
     G::EdgeWeight: fmt::Debug,
     G::NodeWeight: fmt::Debug,
 {
-    let dot = Dot::new(graph);
-    println!("{:?}", dot);
+    println!("{}", get_graph_viz(graph));
+}
+
+pub fn print_graphs_svg<G>(graphs: &[G])
+where
+    G: IntoNodeReferences + IntoEdgeReferences + NodeIndexable + GraphProp,
+    G::EdgeWeight: fmt::Debug,
+    G::NodeWeight: fmt::Debug,
+{
+    println!("digraph {{");
+    graphs.iter().enumerate().for_each(|(i, graph)| {
+        let mut dot = get_graph_viz(graph).replacen("digraph", "subgraph", 1);
+        add_suffix(&mut dot, &format!("{i:03}"));
+        println!("{}", dot);
+    });
+    println!("}}");
+}
+
+fn add_suffix(dot: &mut String, suffix: &str) {
+    let mut map = HashMap::new();
+    for line in dot.lines().skip(1) {
+        if line.contains("->") {
+            break;
+        }
+        let (id, _) = line
+            .trim_start()
+            .split_once(" [")
+            .unwrap_or_else(|| panic!("Error on line: {line}"));
+
+        map.insert(id, format!("{id}{suffix}"));
+    }
+
+    let mut lines = dot.lines();
+    let mut res = lines.next().unwrap().to_string();
+    for line in lines {
+        res.push('\n');
+        if line == "}" {
+            res.push_str(line);
+        } else {
+            let (a, rest) = line
+                .split_once('[')
+                .unwrap_or_else(|| panic!("Error on line: {line}"));
+            let (mut a, mut b) = a
+                .split_once("->")
+                .map(|x| (x.0.trim().to_string(), Some(x.1.trim().to_string())))
+                .unwrap_or_else(|| (a.trim().to_string(), None));
+            let mut a_changed = false;
+            let mut b_changed = b.is_none();
+            for (k, v) in map.iter() {
+                if !a_changed && &a == k {
+                    a = v.to_string();
+                    a_changed = true;
+                }
+                if let Some(b) = b.as_mut() {
+                    if !b_changed && b == k {
+                        *b = v.to_string();
+                        b_changed = true;
+                    }
+                }
+            }
+            write!(res, "{a}").unwrap();
+            if let Some(b) = b {
+                write!(res, " -> {b}").unwrap();
+            }
+            write!(res, " [{rest}").unwrap();
+        }
+    }
+    *dot = res;
 }
