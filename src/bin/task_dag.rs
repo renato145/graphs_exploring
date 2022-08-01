650c21e5386f31134ace639c6b967526ba7e3805
renato <renato.hermoza@pucp.edu.pe>
2022-07-31 20:37:45 -0500

    add graph from list

--- src/bin/task_dag.rs
+++ src/bin/task_dag.rs
@@ -1,39 +1,55 @@
 //! Example of transitive reduction on a graph of tasks
-use graphs_exploring::{get_graph_viz, print_graph_dots};
+use graphs_exploring::{get_graph_viz, print_graph_dots, print_graphs_svg};
+use petgraph::prelude::*;
 use petgraph::{
     algo::{
         toposort,
         tred::{dag_to_toposorted_adjacency_list, dag_transitive_reduction_closure},
     },
+    data::{DataMap, FromElements},
     graph::NodeIndex,
+    visit::{
+        GetAdjacencyMatrix, IntoEdgeReferences, IntoEdges, IntoNodeIdentifiers, IntoNodeReferences,
+    },
     Graph,
 };
 
 fn main() {
-    let mut graph = Graph::<_, ()>::new();
+    let mut graph = Graph::<_, _>::new();
     graph.add_node("A");
     graph.add_node("B");
     graph.add_node("C");
     graph.add_node("D");
     graph.add_node("E");
     graph.extend_with_edges(&[
-        (0, 1),
-        (0, 2),
-        (0, 3),
-        (0, 4),
-        (1, 3),
-        (2, 3),
-        (2, 4),
-        (3, 4),
+        (0, 1, 1),
+        (0, 2, 1),
+        (0, 3, 3),
+        (0, 4, 2),
+        (1, 3, 1),
+        (2, 3, 1),
+        (2, 4, 1),
+        (3, 4, 2),
     ]);
+    // println!("{:?}", graph);
 
     // Transitive reduction
-    let sorted = toposort(&graph, None).unwrap();
+    let sorted = toposort(&graph, None).expect("Not acyclic graph");
     let reduction = {
         let (res, _revmap) = dag_to_toposorted_adjacency_list::<_, NodeIndex>(&graph, &sorted);
         let (res, _revmap) = dag_transitive_reduction_closure(&res);
         res
     };
-    let dots = [get_graph_viz(&graph), get_graph_viz(&reduction)];
+    let edges = reduction
+        .edge_references()
+        .map(|x| (x.source(), x.target()))
+        .collect::<Vec<_>>();
+    let g = Graph::<(), ()>::from_edges(edges);
+
+    let dots = [
+        get_graph_viz(&graph),
+        get_graph_viz(&reduction),
+        get_graph_viz(&g),
+    ];
     print_graph_dots(&dots);
 }
